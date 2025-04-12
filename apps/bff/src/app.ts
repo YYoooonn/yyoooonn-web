c38f579ca8547e6e992eed12b5f31e48ccab153c
Jonguk, Yoon <89994165+YYoooonn@users.noreply.github.com>
2025-04-13 01:22:14 +0900

    chore: add root routing response

--- apps/bff/src/app.ts
+++ apps/bff/src/app.ts
@@ -5,4 +5,16 @@ const app: express.Application = express();
 app.use(express.json());
 app.use("/auth", authRouter);
 
+// Serve static files from the public directory
+app.get("/", (req, res) => {
+  res.send({
+    title: "yyoooonn api server",
+    status: "working in progress",
+    requestIp: req.ip,
+    requestUrl: req.url,
+    requestMethod: req.method,
+    requestDate: new Date(),
+  });
+});
+
 export default app;
--- apps/bff/src/server.ts
+++ apps/bff/src/server.ts
@@ -1,20 +1,18 @@
 import http from "http";
-import express from "express"; // 추가
+import expressServer from "./app"; // 추가
 import { expressMiddleware } from "@apollo/server/express4";
 import { createApolloServer } from "./graphql/server";
 import { createContext } from "./graphql/context";
 
-const app = express(); // app을 express 애플리케이션으로 설정
-
-// JSON 요청을 처리하기 위한 미들웨어 추가
-app.use(express.json());
+// // JSON 요청을 처리하기 위한 미들웨어 추가
+// app.use(express.json());
 
 async function startServer() {
-  const httpServer = http.createServer(app);
+  const httpServer = http.createServer(expressServer);
   const apolloServer = await createApolloServer();
 
   // express 미들웨어로 ApolloServer를 사용
-  app.use(
+  expressServer.use(
     "/graphql",
     expressMiddleware(apolloServer, {
       context: createContext,
