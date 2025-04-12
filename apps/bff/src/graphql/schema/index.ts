3616372d1834bc72ed5a09effb531fef759b26aa
Jonguk, Yoon <89994165+YYoooonn@users.noreply.github.com>
2025-04-13 01:21:22 +0900

    fix: seperate path to local test env with docker env

--- apps/bff/src/graphql/schema/index.ts
+++ apps/bff/src/graphql/schema/index.ts
@@ -5,11 +5,18 @@ import path from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const prod = process.env.NODE_ENV === "production";
+
 // docker 및 로컬에서 사용하기 위한 상대 경로
-const sharedGraphqlPath = path.join(
-  __dirname,
-  "../node_modules/@repo/shared/dist/graphql/schema/**/*.graphql",
-);
+const sharedGraphqlPath = prod
+  ? path.join(
+      __dirname,
+      "../node_modules/@repo/shared/dist/graphql/schema/**/*.graphql",
+    )
+  : path.join(
+      __dirname,
+      "../../../node_modules/@repo/shared/src/graphql/schema/**/*.graphql",
+    );
 
 console.log(`Loading GraphQL schema from ${sharedGraphqlPath}`);
 
